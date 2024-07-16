0980d6c50f21398d5b69c82fcfff059ed88ffd67
popsickle101 <165865470+popsickle101@users.noreply.github.com>
2024-07-16 16:49:00 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3001
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static("./dist/"));
 
 let persons = [
     { "id": "1", "name": "Arto Hellas", "number": "040-123456" },
